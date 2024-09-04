4df4173c747d382f204cf869fb905b5ea512fade
farazal9 <farazxwave@gmail.com>
2024-09-04 14:50:36 +0500

    Your commit message here

---
+++ script.js
@@ -0,0 +1,6 @@
+// let getCard = document.getElementById("card");
+// let getMrIndia = document.getElementById("mrIndia");
+
+// getCard.addEventListener("mouseenter",function(){
+// getMrIndia.style.display = "block"
+// });
