ed2063669e099c5b80cef3b36d606b4f1605298d
farazal9 <farazxwave@gmail.com>
2024-09-05 14:29:30 +0500

    hero section changes

--- script.js
+++ script.js
@@ -4,3 +4,21 @@
 // getCard.addEventListener("mouseenter",function(){
 // getMrIndia.style.display = "block"
 // });
+
+let getcardText = document.querySelectorAll("#cardText");
+
+for(let i = 0; i<getcardText.length; i++){
+    getcardText[i].style.fontSize = "14px";
+}
+
+
+let getimgCard = document.getElementById("imgCard");
+getimgCard.style.backgroundColor = "#EEF1F6";
+
+
+
+let getgetCardTwo = document.getElementById("getCardTwo");
+
+getgetCardTwo.style.backgroundColor = "#222934";
+
+
