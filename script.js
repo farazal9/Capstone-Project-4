9d07d44b36feea2274aa0611169159d7a415dc51
farazal9 <farazxwave@gmail.com>
2024-09-09 13:41:11 +0500

    home completed

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// let getCard = document.getElementById("card");
-// let getMrIndia = document.getElementById("mrIndia");
-
-// getCard.addEventListener("mouseenter",function(){
-// getMrIndia.style.display = "block"
-// });
 
 let getcardText = document.querySelectorAll("#cardText");
 
 for(let i = 0; i<getcardText.length; i++){
     getcardText[i].style.fontSize = "14px";
 }
+
+
+let gettwelve = document.querySelectorAll("#twelve");
+
+for(let z = 0; z<gettwelve.length; z++){
+    gettwelve[z].style.fontSize = "12px"
+
+};
+
+
+let getbgFirst =document.querySelectorAll(".bg-frst");
+for(let y = 0; y<getbgFirst.length; y++){
+getbgFirst[y].style.backgroundColor = "#F5F7FA"
+}
+
+
 
 
 let getimgCard = document.getElementById("imgCard");
@@ -22,3 +32,15 @@ let getgetCardTwo = document.getElementById("getCardTwo");
 getgetCardTwo.style.backgroundColor = "#222934";
 
 
+
+let getFooter = document.getElementById("footer");
+
+getFooter.style.backgroundColor = "#222934";
+
+
+
+
+
+let getInputBorder = document.getElementById("inputBorder");
+
+getInputBorder.style.border = "1px solid #E1D9D1";
