2a0720a1546dd2a542d3e8799941b10964e79e7b
farazal9 <farazxwave@gmail.com>
2024-09-10 15:02:00 +0500

    sign in changes

---
+++ sign in/signin.js
@@ -0,0 +1,7 @@
+let getPinkBg = document.getElementById("pinkBg");
+getPinkBg.style.backgroundColor ="#F55266";
+
+
+let getimgBg = document.getElementById("imgBg");
+
+getimgBg.style.backgroundColor = "#DAE8FA";
